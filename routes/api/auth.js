3d59fd43a8d9f61a2cb87c76f9b9311084295f93
Nikhar <bhavsarnikhar21@gmail.com>
2019-12-02 12:36:21 -0800

    Implemented Auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,85 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+const User = require('../../modals/User');
 
 // @Route  GET api/auth
 // @Desc   Test route
 // @Access Public
 
-router.get('/', (req, res) => {
-  res.send('auth route');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('server error');
+  }
 });
+
+// @Route  POST api/auth
+// @Desc   Authenticate User and get token
+// @Access Public
+
+router.post(
+  '/',
+  [
+    check('email', 'Please provide a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //See if user exist
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ error: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      //Return jsonwebtoken
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator/check');
 const router = express.Router();
+const gravtar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const User = require('../../modals/User');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
 // @Route  POST api/users
 // @Desc   Register User
@@ -18,10 +23,64 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+    }
+
+    //See if user exist
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ error: [{ msg: 'User already exist' }] });
+      }
+
+      //Get users gravtar
+
+      const avatar = gravtar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      //Encrypt password
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      //Return jsonwebtoken
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
     }
   }
 );
